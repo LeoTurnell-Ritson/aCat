2ae0467ee9f4fd8ec1dc7c8639c5c53589f152f4
L-alt-del <l.turnell.ritson@gmail.com>
2023-10-16 20:54:47 +0100

    templates header and source files for version 0.1

---
+++ etc/templates/cattemplate.c
@@ -0,0 +1,64 @@
+/* ============================= aCat ================================ **
+**
+** academic Cartesian Algebraic Toolset
+** https://github.com/LeoTurnell-Ritson/aCat
+**
+** @author Copyright (C) 2023  Leo Turnell-Ritson
+** @version 0.1
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA.
+** ==================================================================== */
+
+
+/* ==================================================================== */
+/* ========================== include files =========================== */
+/* ==================================================================== */
+
+/* Inclusion of system and local header files goes here */
+
+
+/* ==================================================================== */
+/* ============================ constants ============================= */
+/* ==================================================================== */
+
+/* #define and enum statements go here */
+
+
+/* ==================================================================== */
+/* ======================== global variables ========================== */
+/* ==================================================================== */
+
+/* Global variables definitions go here */
+
+
+/* ==================================================================== */
+/* ========================== private data ============================ */
+/* ==================================================================== */
+
+/* Definition of private datatypes go here */
+
+
+/* ==================================================================== */
+/* ====================== private functions =========================== */
+/* ==================================================================== */
+
+/* Function prototypes for private (static) functions go here */
+
+
+/* ==================================================================== */
+/* ===================== All functions by section ===================== */
+/* ==================================================================== */
+
+/* Functions definitions go here, organised into sections */
---
+++ etc/templates/cattemplate.h
@@ -0,0 +1,62 @@
+/* ============================= aCat ================================ **
+**
+** academic Cartesian Algebraic Toolset
+** https://github.com/LeoTurnell-Ritson/aCat
+**
+** @author Copyright (C) 2023  Leo Turnell-Ritson
+** @version 0.1
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA.
+** ==================================================================== */
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifndef CATXXX_H
+#define CATXXX_H
+
+/* ==================================================================== */
+/* ========================== include files =========================== */
+/* ==================================================================== */
+
+/* Inclusion of system and local header files goes here */
+
+/* ==================================================================== */
+/* ============================ constants ============================= */
+/* ==================================================================== */
+
+/* #define and enum statements go here */
+
+
+/* ==================================================================== */
+/* ========================== public data ============================= */
+/* ==================================================================== */
+
+/* Definition of public (external) data types go here */
+
+
+/* ==================================================================== */
+/* ======================= public functions =========================== */
+/* ==================================================================== */
+
+/* Function prototypes for public (external) functions go here */
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
