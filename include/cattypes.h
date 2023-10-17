f8bf7e8bc83b3c851ac23aca860249d9b6737597
L-alt-del <l.turnell.ritson@gmail.com>
2023-10-17 01:53:02 +0100

    Initial includes with some common syntax/macros.

---
+++ include/cattypes.h
@@ -0,0 +1,54 @@
+/* ***************************** aCat ******************************** **
+**
+** @file cattypes
+** @description
+**
+** @author Copyright (C) 2023  Leo Turnell-Ritson
+** @version 0.1
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA.
+**
+** ******************************************************************** */
+
+#include "catcommon.h"
+
+BEGIN_C_DECLS
+
+#ifndef CATTYPES_H
+#define CATTYPES_H
+
+/* ******************************************************************** */
+/* **************************** constants ***************************** */
+/* ******************************************************************** */
+
+typedef enum CatErrorCode {
+  CAT_SUCCESS = 0,
+  CAT_FAILURE = 1              /* Do not use. */
+} CatErrorCode;
+
+typedef enum CatBool {
+  CAT_FALSE = 0,
+  CAT_TRUE = 1
+} CatBool;
+
+typedef short int CatFlag;
+typedef long      CatInt;
+typedef double    CatScalar;
+
+#endif
+
+END_C_DECLS
+
+/* ******************************************************************** */
