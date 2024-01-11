0bf0bd83329ff4b0cddbaac922f9d3db4c2cf23f
Leo Turnell-Ritson <l.turnell.ritson@gmail.com>
2024-01-11 22:27:25 +0000

    Added memory hash table conflicts

--- src/system/debug/mtable.c
+++ src/system/debug/mtable.c
@@ -1,14 +1,16 @@
+#include <catstring.h>
 #include <cat/private/catimpl.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-#define DEBUG_MEMORY_TABLE_SIZE 1213
+#define DEBUG_MEMORY_TABLE_SIZE 1031
 
 struct __mnode {
-        const char  *key;
-        const char  *func;
-        const char  *file;
-        int         line;
-        struct __mnode *next;
+        int               line;
+        char             *key;
+        char             *func;
+        char             *file;
+        struct __mnode   *next;
 };
 
 struct __mtable {
@@ -17,51 +19,110 @@ struct __mtable {
 
 static struct __mtable table;
 
+static label_t __free_count = 0;
+
+static label_t __malloc_count = 0;
+
 static void __HashKey(const char *key, label_t *loc)
 {
-        label_t hash;
-        int c;
+        label_t   hash;
+        int       c;
 
         hash = 5381;
         while ((c = *key++)) hash = ((hash << 5) + hash) + c;
         *loc = hash % DEBUG_MEMORY_TABLE_SIZE;
 }
 
-void __MemoryPush(const char *file, const char *func, const int line, const char *key)
+static void __NodeCpyItem(const char *item, char **elem)
 {
-        label_t loc;
-        struct __mnode *node;
+        int   len;
 
-        node = (struct __mnode *)malloc(sizeof (struct __mnode));
-        node->next = NULL;
-        node->key = key;
-        node->file = file;
-        node->func = func;
-        node->line = line;
-        __HashKey(key, &loc);
-        if (!(table.nodes[loc]))
-                table.nodes[loc] = node;
-        else
-                CatCheckNoReturn(CAT_FALSE, MPI_COMM_SELF, CAT_ERR_NOT_IMPLEMENTED, "Node key conflicts not implemented");
+        len = strlen(item);
+        *elem = (char *)malloc((len + 1) * sizeof (char));
+        __malloc_count++;
+        strcpy(*elem, item);
 }
 
-void __MemoryPop(const char *key)
+void __MemoryPush(const char *file, const char *func, const int line, const char *ptr)
 {
-        label_t loc;
 
-        __HashKey(key, &loc);
+        label_t           loc;
+        struct __mnode   *new;
+
+        new = (struct __mnode *)malloc(sizeof (struct __mnode));
+        __malloc_count++;
+        new->line = line;
+        new->next = NULL;
+        __NodeCpyItem(file, &(new->file));
+        __NodeCpyItem(func, &(new->func));
+        __NodeCpyItem(ptr, &(new->key));
+        __HashKey(ptr, &loc);
+        if (!(table.nodes[loc])) {
+                table.nodes[loc] = new;
+        } else {
+                struct __mnode   *cur, *prv;
+
+                prv = table.nodes[loc];
+                cur = table.nodes[loc]->next;
+                while (cur) {
+                        prv = cur;
+                        cur = cur->next;
+                }
+                prv->next = new;
+        }
+
+}
+
+void __MemoryPop(const char *ptr)
+{
+        label_t           loc;
+        struct __mnode   *cur, *nxt, *prv;
+
+        __HashKey(ptr, &loc);
         CatCheckNoReturn(table.nodes[loc], MPI_COMM_SELF, CAT_ERR_DEBUG_MEMORY_FREE, " ");
-        free(table.nodes[loc]);
-        table.nodes[loc] = NULL;
+        cur = table.nodes[loc];
+        if (strcmp(ptr, cur->key) == 0) {
+                nxt = cur->next;
+                free(cur->key);
+                free(cur->file);
+                free(cur->func);
+                free(cur);
+                __free_count += 4;
+                table.nodes[loc] = nxt;
+        } else {
+                prv = cur;
+                cur = cur->next;
+                while (cur) {
+                        if (strcmp(ptr, cur->key) == 0) {
+                                prv ->next = cur->next;
+                                free(cur->key);
+                                free(cur->file);
+                                free(cur->func);
+                                free(cur);
+                                __free_count += 4;
+                                cur = NULL;
+                        } else {
+                                prv = cur;
+                                cur = cur->next;
+                        }
+                }
+        }
 }
 
 void __MemoryCheck(void)
 {
-        label_t i;
+        label_t           i;
+        struct __mnode   *cur;
 
         for (i = 0; i < DEBUG_MEMORY_TABLE_SIZE; i++) {
-                if (table.nodes[i]) {
-                        __CatErrorMeowf(MPI_COMM_SELF, table.nodes[i]->file, table.nodes[i]->func, table.nodes[i]->line, "leak", CAT_ERR_MEMORY_LEAK, " ");
+                cur = table.nodes[i];
+                while (cur) {
+                        __CatErrorMeowf(MPI_COMM_SELF, cur->file, cur->func, cur->line, "leak", CAT_ERR_MEMORY_LEAK, " ");
+                        cur = cur->next;
                 }
         }
+/*
+  CatCheckNoReturn(__malloc_count == __free_count, MPI_COMM_SELF, CAT_ERR_DEBUG_MEMORY_FREE,
+  "internal memory table error, __malloc_count(%d) != __free_count(%d)", __malloc_count, __free_count);
+*/
 }
