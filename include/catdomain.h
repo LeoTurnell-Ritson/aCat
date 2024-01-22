0f446d45d5af522586018328efc36049909f97a8
Leo Turnell-Ritson (Advanced Research Computing) <l.n.turnell-ritson@bham.ac.uk>
2024-01-22 17:16:28 +0000

    typo

--- include/catdomain.h
+++ include/catdomain.h
@@ -4,8 +4,8 @@
 
 #define DOMAIN_BLOCK "block"
 
-CAT_EXTERN CAT_RETURN_CODE DomainCreate(MPI_Comm, domain_p *);
-CAT_EXTERN CAT_RETURN_CODE DomainDestroy(domain_p *);
-CAT_EXTERN CAT_RETURN_CODE DomainSetFromOptions(domain_p);
-CAT_EXTERN CAT_RETURN_CODE DomainSetType(char *, domain_p);
-CAT_EXTERN CAT_RETURN_CODE DomainSetDimensions(label_t, domain_p);
+CAT_EXTERN CAT_RETURN_CODE DomainCreate(MPI_Comm, DOMAIN **);
+CAT_EXTERN CAT_RETURN_CODE DomainDestroy(DOMAIN **);
+CAT_EXTERN CAT_RETURN_CODE DomainSetFromOptions(DOMAIN *);
+CAT_EXTERN CAT_RETURN_CODE DomainSetType(char *, DOMAIN *);
+CAT_EXTERN CAT_RETURN_CODE DomainSetDimensions(int, DOMAIN *);
